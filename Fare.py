2182f5bad6c8f5adccc9356da8381050831d418b
Daksh-coder-oss <vv0039190@gmail.com>
2025-02-15 10:17:33 +0000

    Add files via upload

---
+++ Fare.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self):
+        def fare(self,x*100):
+            x=input("Please enter the seating capacity")
+class Bus(Vehicle):
+    def __init__():
+        def new_fare(self,fare+10/100*fare):
+            print(fare)
+print(Vehicle())
+
